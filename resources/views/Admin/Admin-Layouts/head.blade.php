e5f7daacfe591e75799b0571ee5974d836df0f29
Amrit Kafle <amk.amritan@gmail.com>
2019-04-06 01:17:22 +0545

    Second Commit

--- resources/views/Admin/Admin-Layouts/head.blade.php
+++ resources/views/Admin/Admin-Layouts/head.blade.php
@@ -1,5 +1,6 @@
 <div class="head">
 	<div class="col-sm-6" align="right">
 		<i style="color: white; font-size: 15px;"  class='far fa-envelope'></i>
+		<h1>Helllo Test</h1>
 	</div>
 </div>
--- resources/views/Admin/app.blade.php
+++ resources/views/Admin/app.blade.php
@@ -5,10 +5,7 @@
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="css/custome.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <body>
 	<div class="container-fluid">
 		<div class="row">
@@ -19,6 +16,6 @@
 		@yield('content')
 
 	</div>
-	
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
