119ea34d1eb1ae20d9551a9b14e6e34b4829643d
Amrit Kafle <amk.amritan@gmail.com>
2019-09-20 23:48:39 +0545

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -49203,8 +49203,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/eramritkafle/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/eramritkafle/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/news/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/news/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
